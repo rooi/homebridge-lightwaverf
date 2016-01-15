e6adf5d85dfcd972f3dc71fc0808aeda527ab23d
Roy <roy.arents@gmail.com>
2016-01-15 12:25:35 +0100

    ...

--- index.js
+++ index.js
@@ -150,7 +150,9 @@ LightWaveRFAccessory.prototype = {
       case 'power':
         if (value > 0) {
             if(this.isDimmer) {
+                console.log(this.previousPercentage);
                 if(this.previousPercentage < 5 ) this.previousPercentage = 100; // Prevent very low last states
+                console.log(this.previousPercentage);
                 this.api.setDeviceDim(this.roomId,this.deviceId,this.previousPercentage,callback);
                 this.status = this.previousPercentage;
             } else {
@@ -165,6 +167,7 @@ LightWaveRFAccessory.prototype = {
         break;
       case 'brightness':
         this.previousPercentage = this.status;
+        console.log(this.previousPercentage);
         // Only write when change is larger than 5
         this.status = value;
         if((value % 5) == 0) {
